7bc82b74ff8979cd834b117e5f437dc2676a2125
Нуржан <Studerzhan@gmail.com>
2022-12-05 20:01:01 +0600

    connect with postgres, create user model, user form

--- Mega/settings.py
+++ Mega/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'Mega.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'h13XVk3S6HIJoHj4547l',
+       'HOST': 'containers-us-west-98.railway.app',
+       'PORT': '5642',
+   }
 }
 
 
@@ -116,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+AUTH_USER_MODEL = 'user.CustomUser'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from Mega.user.forms import UserForm
+
+
+class RegisterUser(CreateView):
+    form_class = UserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
